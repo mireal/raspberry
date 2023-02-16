3abb6cb86f961c15a26d79021f3f65cf09513980
mireal <moveloader@yandex.ru>
2023-02-16 07:23:16 +0300

    pickle test
    playing with pickle data storing

--- test.py
+++ test.py
@@ -1,27 +1,41 @@
-import datetime
+import csv
+import pickle
+import os
 
-# import csv
-# from os.path import exists
-# import os
-# for datafile in os.listdir('results'):
-#
-#     fields = ['Date', 'Time', 'Temperature', 'Humidity']
-#
-#     if not exists(f'{datafile}.csv'):
-#         with open(f'{datafile}.csv', 'w', newline='') as file:
-#             csvfile = csv.writer(file)
-#             csvfile.writerow(fields)
-#     with open(f'{datafile}.csv', 'a+', newline='') as file:
-#
-#         csvfile = csv.writer(file)
-#         with open(f'results\{datafile}', 'r+') as textfile:
-#             for line in textfile:
-#                 line = line.split()
-#                 line = line[0], line[1], line[3], line[6]
-#                 csvfile.writerow(line)
-#
-#
-#
+path = '.\\results\\'
+csv_files = [f for f in os.listdir(path)]
+pickle_file = 'data.pickle'
+filtered_file = 'filtered_data.pickle'
 
-last_record = datetime.datetime.now().strftime('%M')
-print(last_record)
+def csv_to_pickle(csv_files, pickle_file):
+    all_rows = []
+    for csv_file in csv_files:
+        with open(os.path.join(path, csv_file), 'r') as csv_f:
+            reader = csv.reader(csv_f)
+            headers = next(reader) # ignore header of csv files
+            for row in reader:
+                all_rows.append(row)
+
+    with open(pickle_file, 'wb') as f:
+        pickle.dump(all_rows, f)
+
+def filter_by_minute(input_file, output_file):
+    with open(input_file, 'rb') as f_in, open(output_file, 'wb') as f_out:
+        headers = pickle.load(f_in)
+        pickle.dump(headers, f_out)
+        last_minute = None
+        for row in pickle.load(f_in):
+            time_parts = row[1].split(':')
+            minute = time_parts[1]
+            if last_minute != minute:
+                pickle.dump(row, f_out)
+                last_minute = minute
+
+
+# filter_by_minute('data.pickle','filtered_data.pickle')
+# csv_to_pickle(csv_files, pickle_file)
+
+with open(filtered_file, 'rb') as file:
+    my_data = pickle.load(file)
+    for row in my_data:
+        print(row)
